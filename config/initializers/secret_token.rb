310427aba9cc8746a63b010c955fdc70c7c585ce
Aidan Corbett <aidan515@gmail.com>
2012-02-22 12:39:37 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicShelf::Application.config.secret_token = '7c240d90fb82ab56d527637027905f8bcb8da855c3fbe876b9508b008a580e9bd3450c204d568d5dc128d6fbb61073a12c80c5d1547fba62e59487a801a564fa'
